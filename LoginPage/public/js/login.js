68b75bfc2b8a26eb095475102b7749aeaaa287e8
Delayman <61342646+Delayman@users.noreply.github.com>
2022-05-14 15:38:57 +0700

    Create Login-Register and Database
    Create Login and Register system including database of it. Add "Register" and "I have an account" button to login.html and register.html

---
+++ LoginPage/public/js/login.js
@@ -0,0 +1,43 @@
+window.onload = pageLoad;
+// const express = require('express');
+// const app = express();
+// const fs = require('fs');
+
+function pageLoad()
+{
+	
+	
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	if (urlParams.get("error")==1){
+		if (window.location.href.split('/').pop()== "register.html"){
+			document.getElementById('errordisplay').innerHTML = "Registration Error!"
+		}else{
+			document.getElementById('errordisplay').innerHTML = "Username or password does not match.";
+
+		}
+		
+	}
+
+	if(window.location.href.split('/').pop()== "register.html")
+	{
+		goLogin();
+	}else
+	{
+		goRegister();
+	}
+}
+
+function goRegister()
+{
+	var regButton = document.getElementById("goRegister").onclick = function(){
+		location.href = "/register.html"
+	};
+}
+
+function goLogin()
+{
+	var loginButton = document.getElementById("goLogin").onclick = function(){
+		location.href = "/login.html"
+	};
+}
